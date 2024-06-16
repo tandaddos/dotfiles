4e3d43013ecca9b78328dd7e09f9bb3bc36e3853
tandaddos <tandaddos@gmail.com>
2024-06-16 12:29:36 -0700

    alias which for non-zsh

--- common/common_aliases.zsh
+++ common/common_aliases.zsh
@@ -18,10 +18,11 @@
 alias _='sudo '
 alias md='mkdir -p'
 alias rd=rmdir
-# alias which='alias | /usr/bin/which --tty-only --read-alias --show-tilde --show-dot'
 if [[ "$SHELL" = *"zsh"* ]]; then
   alias which-command=whence
   alias history=omz_history
+else
+	alias which='alias | /usr/bin/which --tty-only --read-alias --show-tilde --show-dot'
 fi
 alias run-help=man
 
